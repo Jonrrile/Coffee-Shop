f23c6409779b3092750812f31f6b12a3c9262a49
Jon Riley <jonrrile@ut.utm.edu>
2020-09-30 11:22:43 -0500

    can list the beans upon run affordance. Button for add put in place

--- CoffeeShop/js-app/main.js
+++ CoffeeShop/js-app/main.js
@@ -2,12 +2,35 @@ const url = "https://localhost:5001/api/beanvariety/";
 
 const button = document.querySelector("#run-button");
 button.addEventListener("click", () => {
-    getAllBeanVarieties()
-        .then(beanVarieties => {
-            console.log(beanVarieties);
-        })
+   // getAllBeanVarieties()
+     //   .then(beanVarieties => {
+    //      console.log(beanVarieties);
+    showAll();
+        //})
 });
 
 function getAllBeanVarieties() {
     return fetch(url).then(resp => resp.json());
+}
+
+function htmlConverter(bean) {
+    const beanHTML =
+        `<div>
+            <ul>
+                <li>
+                    ${bean.name}
+                </li>
+            </ul>
+        </div>`
+    return beanHTML
+};
+
+function showAll() {
+    getAllBeanVarieties()
+        .then(beans => {
+            for (let bean of beans) {
+                let html = htmlConverter(bean);
+                document.querySelector(".beans").innerHTML += html;
+            }
+        })
 }
