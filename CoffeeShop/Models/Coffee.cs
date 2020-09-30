bc13157494f6c4795ad4824e36b270f766db7ee3
Jon Riley <jonrrile@ut.utm.edu>
2020-09-30 13:42:37 -0500

    progress on coffee

---
+++ CoffeeShop/Models/Coffee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public BeanVariety Bean { get; set; }
+        public int BeanVarietyId { get; set; }
+    }
+}
